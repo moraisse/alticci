6d332efe6e00489f815e60fd9b44db7ea577ef6f
moraisse <moraisse@gmail.com>
2022-11-13 17:03:44 +0000

    test

--- src/main/java/com/smorais/altice/alticci/AlticciApplication.java
+++ src/main/java/com/smorais/altice/alticci/AlticciApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AlticciApplication {
 
     public static void main(String[] args) {
+        System.out.println("teste");
         SpringApplication.run(AlticciApplication.class, args);
     }
+
 
 }
